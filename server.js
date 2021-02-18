3b44c555b927c8088f31169478c39e994976f595
velezconde <s.esteban125@gmail.com>
2021-02-18 10:04:32 -0500

    initial file setup

---
+++ server.js
@@ -0,0 +1,5 @@
+const  express  =  require ( 'express' ) ; 
+aplicación constante  = express ( ) ;  
+aplicación . listen ( 8000 , function ( ) { 
+console . log ( "el servidor se está ejecutando" ) 
+} )
